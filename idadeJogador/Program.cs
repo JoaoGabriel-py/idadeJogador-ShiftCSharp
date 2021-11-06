915777c30cd98adbfffe39a4c5ea10371063b72f
JoaoGabriel-py <joao.g.castro20@gmail.com>
2021-11-06 13:32:49 -0300

    Desvio condicional simples

---
+++ idadeJogador/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace idadeJogador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade;
+
+            Console.WriteLine("Digite a idade do jogador:");
+            idade = Convert.ToByte(Console.ReadLine());
+
+            if (idade >= 12)
+            {
+                Console.WriteLine("Você pode jogar!");
+            } else
+            {
+                Console.WriteLine("Você não possui a idade mínima!");
+            }
+        }
+    }
+}
